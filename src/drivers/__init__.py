fd360a22d34551f1b6bdf4dc93e98f65ec06c5c3
H.C.Li <2056524536@qq.com>
2026-07-13 01:15:18 +0800

    更新 __init__.py

--- src/drivers/__init__.py
+++ src/drivers/__init__.py
@@ -1,13 +1,36 @@
 from .env_check import is_real_jetson, is_sim_env, SIM_LOG
 from .camera import StereoCamera
-from .gpio import GPIO
+from .gpio import BaseGPIO
+from .button import Button
+from .vibrator import Vibrator
+from .serial import BaseSerial
+from .imu import IMU
+from .gps import GPS
+from .lte import LTE
 from .bluetooth import BluetoothManager
+from .audio import AudioManager
+from .wifi import WiFiManager
+from .net import BaseNet, NetManager
+from .i2c import BaseI2C
+from .ups import UPS
 
 __all__ = [
     "is_real_jetson",
     "is_sim_env",
     "SIM_LOG",
     "StereoCamera",
-    "GPIO"
+    "BaseGPIO"
+    "Button"
+    "Vibrator"
+    "BaseSerial"
+    "IMU"
+    "GPS"
+    "LTE"
     "BluetoothManager"
+    "AudioManager"
+    "WiFiManager"
+    "BaseNet"
+    "NetManager"
+    "BaseI2C"
+    "UPS"
 ]
